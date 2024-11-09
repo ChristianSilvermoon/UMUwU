6847c0c65ffdc98d7fb3df873e127a1f26a10b1d
Christian "Krissy" Silvermoon <30162808+ChristianSilvermoon@users.noreply.github.com>
2024-11-09 00:25:52 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,31 @@
+# UMUwU
+Wrapper script to make standalone [umu-launcher](https://github.com/Open-Wine-Components/umu-launcher) more convenient to use.
+
+Yes, the name is intended to be a meme.
+
+Defaults
+- This will attempt to use the highest version number of GE-Proton it finds under `~/.steam/steam/compatibilitytools.d/`
+- This `$XDG_DATA_HOME/UMUwU/prefix/default` as a Proton Prefix
+
+## Hopes & Dreams
+Eventually, a goal is to make it so that setting a particular prefix for the hash of a specific path or executable file works.
+
+MAYBE storing the intended info like prefix, proton version, GAMEID, etc in extended filesystem attributes would be reasonable?
+
+Extended Filesystems Attributes are easily lost when moving files from one drive to another so that's worth noting.
+
+MAYBE a history system and/or shortcut system to make re-launching previously done things easier?
+
+This all revolves around finding the motivation to actually do it.
+
+## Options
+
+| OPTION                | What it does                                                                      |
+| :-------------------- | --------------------------------------------------------------------------------: |
+| `--appdata`, `-a`     | `xdg-open` the AppData folder for the current Prefix                              |
+| `--prefix=<PATH>`     | Use PATH as prefix, or `$XDG_DATA_HOME/UMUwu/prefix/NAME` if PATH contains no `/` |
+| `--proton=<PATH>`     | Use PATH as `$PROTONPATH`                                                         |
+| `--prefix-here`, `-P` | Use `./umuwu-prefix` as `$WINEPREFIX`                                             |
+| `--simulate`, `-s`    | Display variables and exit. Don't actually launch anything.                       |
+| `--`                  | Do not process anymore arguments for `umuwu`                                      |
+| `--help`, `-?`        | Display help message                                                              |
---
+++ umuwu.sh
@@ -0,0 +1,98 @@
+#!/bin/bash
+
+# Initialize XDG Vars
+: ${XDG_DATA_DIR:=$HOME/.local/share}
+: ${XDG_CONFIG_HOME:=$HOME/.config}
+: ${XDG_CACHE_HOME:=$HOME/.cache}
+: ${XDG_STATE_HOME:=$HOME/.local/state}
+
+UMUWU_DATA="${XDG_DATA_DIR}/UMUwU"
+
+
+# -----------
+# UMU Example
+# -----------
+#   export WINEPREFIX=$HOME/Games/epic-games-store
+#   export GAMEID=umu-dauntless
+#   export STORE=egs 
+#   export PROTONPATH="$HOME/.steam/steam/compatibilitytools.d/GE-Proton8-28" 
+#   exec umu-run "$HOME/Games/epic-games-store/drive_c/Program Files (x86)/Epic Games/Launcher/Portal/Binaries/Win32/EpicGamesLauncher.exe" -opengl -SkipBuildPatchPrereq
+# -----------
+
+: ${WINEPREFIX:=$UMUWU_DATA/prefix/default}
+: ${GAMEID:=umuwu-launcher}
+: ${STORE:=}
+
+err() { echo "$@" 1>&2; }
+
+help_msg() {
+	local e="${0##*/}"
+	echo -e "\e[1;32m${e}\e[39m - \e[36mx3 umu is cool~<3, UwU\e[0m"
+	echo -e "\n  Easily manage and run software with Proton prefixes using umu-run\n"
+
+	echo -e "\e[1mUSAGE\e[0m"
+	echo -e "  $e [OPTIONS] [EXE]\n"
+
+	echo -e "\e[1mOPTIONS\e[0m"
+	printf "  %-28s %s\n" \
+		"--appdata, -a"     "Open AppData folder for current Prefix" \
+		"--prefix=<PATH>"   "Use specific prefix directory"          \
+		""                  "  if PATH contains no \"/\", then"      \
+		""                  "  under $UMUWU_DATA/prefix/<NAME>"      \
+		"--simulate, -s"    "Don't actually launch anything"         \
+		"--prefix-here, -P" "Set \$WINEPREIFX to \$PWD/umuwu-prefix" \
+		"--help, -?"        "Display this message"
+}
+
+proton_scan() {
+	mapfile -t PROTON_VERSIONS < <( printf "%s\n" "$HOME/.steam/steam/compatibilitytools.d/GE-Proton"* | sort -Vr )
+	PROTONPATH="${PROTON_VERSIONS[0]}"
+}
+
+arg_proc() {
+	local ai a narg ARGS
+
+	for a in "$@"; do
+		[ "$narg" ] && EXE+=( "$a" ) && continue
+
+		case "$a" in
+			"--help"|"-?") help_msg; exit ;;
+			"--appdata"|"-a") exec xdg-open "$WINEPREFIX/drive_c/users/steamuser/AppData" ;;
+			"--proton="*)
+					PROTONPATH="${a#*=}"
+					[ -d "$PROTONPATH" ] && continue
+					err -e "ERROR, Proton Path does not exist.\n  $PROTONPATH"
+					exit 1
+				;;
+			"--prefix-here"|"-P") WINEPREFIX="./umuwu-prefix" ;;
+			"--prefix="*)
+				WINEPREFIX="${a#*=}"
+				[[ ! $WINEPREFIX =~ / ]] && WINEPREFIX="${UMUWU_DATA}/prefix/${WINEPREFIX}"
+				;;
+			"--simulate"|"-s") UMUWU_SIMULATE=true ;;
+			"--")              narg=1; continue ;;
+			"-"*)              err "Unknown Arg: $a" 1>&2; exit 1 ;;
+			*) EXE+=( "$a" ) ;;
+		esac
+	done
+}
+
+arg_proc "$@"
+[ "$PROTONPATH" ] || proton_scan
+
+
+echo "UMU Config:"
+printf " %-28s : %s\n" \
+	'$PROTONPATH'  "$PROTONPATH" \
+	'$WINEPREFIX'  "$WINEPREFIX" \
+	'$STORE'       "${STORE:-$'\e'[2mN/a$'\e'[0m}" \
+	'$GAMEID'      "$GAMEID"
+printf " %-28s : " "EXECUTE"
+printf -- "\"%s\" " "umu-run" "${EXE[@]}"
+echo
+[ "$UMUWU_SIMULATE" ] && echo -e "-----\nSimuated Only. Exiting.\n-----\n" && exit
+
+# Launch
+export PROTONPATH WINEPREFIX STORE GAMEID
+exec umu-run "${EXE[@]}"
+
